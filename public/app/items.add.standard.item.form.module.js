4870beab7c196b17374e7219f7fff1762d9a900c
freyesdulib <fernando.reyes@du.edu>
2024-08-17 11:16:44 -0600

    added exhibit title to standard item form

--- public/app/items.add.standard.item.form.module.js
+++ public/app/items.add.standard.item.form.module.js
@@ -535,6 +535,9 @@ const itemsAddStandardItemFormModule = (function () {
      */
     obj.init = async function () {
 
+        const exhibit_id = helperModule.get_parameter_by_name('exhibit_id');
+        exhibitsModule.set_exhibit_title(exhibit_id);
+
         helperModule.set_rich_text_editor_config();
         set_rich_text_editors();
 
