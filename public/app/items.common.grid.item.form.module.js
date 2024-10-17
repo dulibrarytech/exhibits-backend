5602685d6f9542c4fb076452a99de693a59b2f21
freyesdulib <fernando.reyes@du.edu>
2024-10-17 10:49:32 -0600

    removed media padding field from grid item common module

--- public/app/items.common.grid.item.form.module.js
+++ public/app/items.common.grid.item.form.module.js
@@ -54,7 +54,7 @@ const itemsCommonGridItemFormModule = (function () {
             item.media_prev = document.querySelector('#item-media-prev').value;
             item.kaltura = document.querySelector('#audio-video').value;
             item.repo_uuid = document.querySelector('#repo-uuid').value;
-            item.media_padding = document.querySelector('#media-padding').value;
+            // item.media_padding = document.querySelector('#media-padding').value;
 
             if (item.media.length === 0 && item.kaltura.length === 0 && item.repo_uuid.length === 0) {
                 document.querySelector('#message').innerHTML = `<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation"></i> Please upload or import a media item</div>`;
