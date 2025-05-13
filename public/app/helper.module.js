8db42423b0d9442d14b5b1b37429304687d584c9
Fernando Reyes <fernando.reyes@du.edu>
2025-05-12 23:22:58 -0600

    updated version

--- public/app/helper.module.js
+++ public/app/helper.module.js
@@ -349,7 +349,7 @@ const helperModule = (function () {
                     card.style.visibility = 'visible';
                 });
 
-            }, 250*2);
+            }, 265*2);
 
         } catch (error) {
             document.querySelector('#message').innerHTML = `<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation"></i> ${error.message}</div>`;
