65b1913f6c4aeb2b4185aa755a6245e6698ba366
Fernando Reyes <fernando.reyes@du.edu>
2025-07-17 17:14:13 -0600

    added locked message to grid item form

--- public/app/items.edit.grid.item.form.module.js
+++ public/app/items.edit.grid.item.form.module.js
@@ -82,6 +82,8 @@ const itemsEditGridItemFormModule = (function () {
         let create_date_time = helperModule.format_date(create_date);
         let update_date_time = helperModule.format_date(update_date);
 
+        helperModule.check_if_locked(record, '#item-submit-card');
+
         if (created_by !== null) {
             item_created += `<em>Created by ${created_by} on ${create_date_time}</em>`;
         }
