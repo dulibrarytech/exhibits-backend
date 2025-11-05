626144da06631df2b221fefd54d71a0c1daf5a1f
Fernando Reyes <fernando.reyes@du.edu>
2025-11-05 12:05:03 -0700

    removed unused code from helper media module

--- public/app/utils/helper.media.module.js
+++ public/app/utils/helper.media.module.js
@@ -334,108 +334,6 @@ const helperMediaModule = (function () {
         return icon_map[alert_type] || 'fa fa-info';
     }
 
-    /**
-     * Gets repo item metadata
-     * @param uuid
-     */
-    /*
-    obj.get_repo_item_data__ = async function (uuid) {
-
-        try {
-
-            const EXHIBITS_ENDPOINTS = endpointsModule.get_exhibits_endpoints();
-            let is_list = true;
-
-            if (uuid === null) {
-                uuid = document.querySelector('#repo-uuid').value;
-                helperMediaModule.clear_media_fields('repo_media');
-                is_list = false;
-            }
-
-            if (uuid.length === 0) {
-                document.querySelector('#message').innerHTML = `<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation"></i> Please enter a Repository UUID</div>`;
-            }
-
-            let token = authModule.get_user_token();
-            let response = await httpModule.req({
-                method: 'GET',
-                url: EXHIBITS_ENDPOINTS.exhibits.repo_items.endpoint.replace(':uuid', uuid),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-access-token': token
-                }
-            });
-
-            if (response !== undefined && response.status === 200) {
-
-                if (response.data.data.is_compound === 1) {
-                    document.querySelector('#repo-item-metadata').innerHTML = `<p style="color:red">Repository compound objects are not supported.</p>`;
-                    return false;
-                }
-
-                const mime_type = response.data.data.mime_type;
-                let item_type;
-                let tn = document.querySelector('#tn');
-                let item_mime_type = document.querySelector('#item-mime-type');
-                let type = document.querySelector('#item-type');
-                let repo_item_metadata = document.querySelector('#repo-item-metadata');
-                let is_repo_item = document.querySelector('#is-repo-item');
-
-                if (tn !== null) {
-                    const tn_url = helperMediaModule.render_repo_thumbnail(response.data.data.thumbnail.data);
-                    tn.innerHTML = `<img src="${tn_url}" alt="thumbnail" height="200" width="200" style="border: solid"">`;
-                }
-
-                if (item_mime_type !== null) {
-                    item_mime_type.value = mime_type;
-                }
-
-                if (mime_type.indexOf('image') !== -1) {
-
-                    item_type = 'image';
-
-                    let alt_text = document.querySelector('#image-alt-text');
-
-                    if (alt_text !== null) {
-                        document.querySelector('#image-alt-text').style.display = 'block';
-                    }
-
-                } else if (mime_type.indexOf('video') !== -1) {
-                    item_type = 'video';
-                } else if (mime_type.indexOf('audio') !== -1) {
-                    item_type = 'audio';
-                } else if (mime_type.indexOf('pdf') !== -1) {
-                    item_type = 'pdf';
-                } else {
-                    item_type = 'Unable to Determine Type';
-                }
-
-                if (type !== null) {
-                    type.value = item_type;
-                }
-
-                if (repo_item_metadata !== null) {
-                    repo_item_metadata.innerHTML = `<p><strong>${response.data.data.title}</strong><br><em>${mime_type}</em></p>`;
-                }
-
-                if (is_repo_item !== null) {
-                    is_repo_item.value = 1;
-                }
-
-                if (is_list === true) {
-                    return response.data.data;
-                }
-
-            } else {
-                document.querySelector('#repo-item-metadata').innerHTML = `<p style="color:red">Metadata record not found in repository.</p>`;
-            }
-
-        } catch (error) {
-            document.querySelector('#message').innerHTML = `<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation"></i> ${error.message}</div>`;
-        }
-    };
-    */
-
     obj.render_repo_thumbnail = function(thumbnail_data_array, mime_type = 'image/jpeg') {
 
         /**
@@ -650,26 +548,6 @@ const helperMediaModule = (function () {
         };
     };
 
-    /**
-     *  Renders repository item thumbnail
-     * @param thumbnail_data_array
-     */
-    /*
-    obj.render_repo_thumbnail__ = function (thumbnail_data_array) {
-
-        try {
-
-            const array_buffer_view = new Uint8Array(thumbnail_data_array);
-            const blob = new Blob([array_buffer_view], {type: 'image/jpeg'});
-            const url_creator = window.URL || window.webkitURL;
-            return url_creator.createObjectURL(blob);
-
-        } catch (error) {
-            document.querySelector('#message').innerHTML = `<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation"></i> ${error.message}</div>`;
-        }
-    };
-    */
-
     obj.clear_media_fields = function(type) {
 
         /**
@@ -802,35 +680,6 @@ const helperMediaModule = (function () {
             return false;
         }
     };
-
-    /*
-    obj.clear_media_fields__ = function (type) {
-
-        if (type === 'uploaded_media') {
-            document.querySelector('#repo-uuid').value = '';
-            document.querySelector('#audio-video').value = '';
-            document.querySelector('#is-kaltura-item').value = 0;
-            document.querySelector('#is-repo-item').value = 0;
-        }
-
-        if (type === 'repo_media') {
-            document.querySelector('#audio-video').value = '';
-            document.querySelector('#is-kaltura-item').value = 0;
-        }
-
-        if (type === 'kaltura_media') {
-            document.querySelector('#repo-uuid').value = '';
-            document.querySelector('#is-repo-item').value = 0;
-        }
-
-        document.querySelector('#item-type').value = '';
-        document.querySelector('#item-mime-type').value = '';
-        document.querySelector('#item-media').value = '';
-        document.querySelector('#item-media-thumbnail-image-display').innerHTML = '';
-        document.querySelector('#item-media-filename-display').innerHTML = '';
-        document.querySelector('#item-media-trash').style.display = 'none';
-    };
-    */
 
     obj.get_kaltura_item_data = async function(entry_id) {
 
@@ -1115,70 +964,6 @@ const helperMediaModule = (function () {
         }
     };
 
-    /**
-     * Gets kaltura item data
-     * @param entry_id
-     */
-    /*
-    obj.get_kaltura_item_data__ = async function (entry_id) {
-
-        try {
-
-            const EXHIBITS_ENDPOINTS = endpointsModule.get_exhibits_endpoints();
-            let is_list = true;
-
-            if (entry_id === null) {
-                entry_id = document.querySelector('#audio-video').value;
-                helperMediaModule.clear_media_fields('kaltura_media');
-                is_list = false;
-            }
-
-            if (entry_id.length === 0) {
-                document.querySelector('#message').innerHTML = `<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation"></i> Please enter a Kaltura ID</div>`;
-            }
-
-            const token = authModule.get_user_token();
-            const response = await httpModule.req({
-                method: 'GET',
-                url: EXHIBITS_ENDPOINTS.exhibits.kaltura_items.endpoint.replace(':entry_id', entry_id),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-access-token': token
-                }
-            });
-
-            if (response !== undefined && response.status === 200) {
-
-                if (response.data.data.item_type === undefined) {
-                    document.querySelector('#kaltura-item-data').innerHTML = `<p style="color:red">${response.data.data.message}</p>`;
-                    return false;
-                }
-
-                let kaltura_item_data = document.querySelector('#kaltura-item-data');
-                kaltura_item_data.innerHTML = `<p>
-                    <strong>${response.data.data.title}</strong><br>
-                     (<em>${response.data.data.item_type}</em>)
-                    <!--<small>${response.data.data.description}</small><br>-->
-                    </p>`;
-
-                document.querySelector('#kaltura-thumbnail').src = response.data.data.thumbnail;
-                document.querySelector('#kaltura-item-type').value = response.data.data.item_type;
-                document.querySelector('#kaltura-thumbnail').style.visibility = 'visible';
-
-                if (is_list === true) {
-                    return response.data.data;
-                }
-
-            } else {
-                document.querySelector('#repo-item-metadata').innerHTML = `<p style="color:red">Metadata record not found in repository.</p>`;
-            }
-
-        } catch (error) {
-            document.querySelector('#message').innerHTML = `<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation"></i> ${error.message}</div>`;
-        }
-    };
-    */
-
     obj.toggle_alt_text = function() {
 
         /**
@@ -1403,31 +1188,6 @@ const helperMediaModule = (function () {
         }
     };
 
-    /**
-     * Toggles alt text field based on whether it is decorative or not
-     */
-    /*
-    obj.toggle_alt_text__ = function () {
-
-        try {
-
-            let toggle_elem = document.querySelector('#item-alt-text-input');
-            let is_decorative_toggle = toggle_elem.disabled;
-
-            if (is_decorative_toggle === false) {
-                toggle_elem.disabled = true;
-            } else if (is_decorative_toggle === true) {
-                toggle_elem.disabled = false;
-            }
-
-            return false;
-
-        } catch (error) {
-            document.querySelector('#message').innerHTML = `<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation"></i> ${error.message}</div>`;
-        }
-    };
-    */
-
     obj.set_alt_text = function(record) {
 
         /**
@@ -1607,6 +1367,7 @@ const helperMediaModule = (function () {
      * Clear alt text field
      */
     obj.clear_alt_text = function() {
+
         try {
             const alt_text_input = document.querySelector('#item-alt-text-input');
             const is_decorative_checkbox = document.querySelector('#is-alt-text-decorative');
@@ -1634,6 +1395,7 @@ const helperMediaModule = (function () {
      * Get alt text data for submission
      */
     obj.get_alt_text_data = function() {
+
         try {
             const alt_text_input = document.querySelector('#item-alt-text-input');
             const is_decorative_checkbox = document.querySelector('#is-alt-text-decorative');
@@ -1735,26 +1497,6 @@ const helperMediaModule = (function () {
             };
         }
     };
-
-    /**
-     * Set alt text
-     * @param record
-     */
-    /*
-    obj.set_alt_text__ = function (record) {
-
-        document.querySelector('#image-alt-text').style.display = 'block';
-
-        if (record.is_alt_text_decorative === 1) {
-            document.querySelector('#is-alt-text-decorative').checked = true;
-            let toggle_elem = document.querySelector('#item-alt-text-input');
-            toggle_elem.disabled = true;
-        } else {
-            document.querySelector('#is-alt-text-decorative').checked = false;
-            document.querySelector('#item-alt-text-input').value = helperModule.unescape(record.alt_text);
-        }
-    };
-    */
 
     obj.delete_thumbnail_image = function () {
 
@@ -2839,131 +2581,6 @@ const helperMediaModule = (function () {
             return false;
         }
     };
-
-    /*
-    obj.display_media_fields_common__ = async function (record) {
-
-        const EXHIBITS_ENDPOINTS = endpointsModule.get_exhibits_endpoints();
-        let thumbnail_fragment = '';
-        let thumbnail_url = '';
-        let wrap_text = document.querySelector('#wrap-text');
-        let media_padding = document.querySelector('#media-padding');
-        let embed_item = document.querySelector('#embed-item');
-
-        document.querySelector('#item-description-input').value = helperModule.unescape(record.description);
-        document.querySelector('#item-caption-input').value = helperModule.unescape(record.caption);
-        document.querySelector('#pdf-open-to-page').value = record.pdf_open_to_page;
-
-        if (embed_item) {
-            if (record.is_embedded === 1) {
-                embed_item.checked = true;
-            } else {
-                embed_item.checked = false;
-            }
-        }
-
-        if (wrap_text) {
-            if (record.wrap_text === 1) {
-                wrap_text.checked = true;
-            } else {
-                wrap_text.checked = false;
-            }
-        }
-
-        if (media_padding) {
-
-            if (record.media_padding === 1) { // padding
-                media_padding.checked = false;
-            } else if (record.media_padding === 0) { // no padding
-                media_padding.checked = true;
-            }
-        }
-
-        if (record.media.length > 0) {
-
-            if (record.is_repo_item === 0 && record.is_kaltura_item === 0) {
-
-                if (record.mime_type.indexOf('image') !== -1) {
-
-                    thumbnail_url = EXHIBITS_ENDPOINTS.exhibits.exhibit_media.get.endpoint.replace(':exhibit_id', record.is_member_of_exhibit).replace(':media', record.media);
-                    thumbnail_fragment = `<p><img src="${thumbnail_url}" height="200" ></p>`;
-                    helperMediaModule.set_alt_text(record);
-
-                } else if (record.mime_type.indexOf('video') !== -1) {
-                    thumbnail_url = '/exhibits-dashboard/static/images/video-tn.png';
-                    thumbnail_fragment = `<p><img src="${thumbnail_url}" height="200" ></p>`;
-                } else if (record.mime_type.indexOf('audio') !== -1) {
-                    thumbnail_url = '/exhibits-dashboard/static/images/audio-tn.png';
-                    thumbnail_fragment = `<p><img src="${thumbnail_url}" height="200" ></p>`;
-                } else if (record.mime_type.indexOf('pdf') !== -1) {
-                    thumbnail_url = '/exhibits-dashboard/static/images/pdf-tn.png';
-                    thumbnail_fragment = `<p><img src="${thumbnail_url}" height="200" ></p>`;
-                    document.querySelector('#toggle-open-to-page').style.visibility = 'visible';
-                } else {
-                    console.log('Unable to Determine Type');
-                }
-
-                document.querySelector('#item-media-trash').style.display = 'inline';
-                document.querySelector('#item-media-filename-display').innerHTML = `<span style="font-size: 11px">${record.media}</span>`;
-            }
-
-            document.querySelector('#item-type').value = record.item_type;
-
-            if (record.is_repo_item === 1) {
-
-                document.getElementById('upload-media-tab').classList.remove('active');
-                document.getElementById('import-repo-media-tab').classList.add('active');
-                document.getElementById('upload-media').classList.remove('active');
-                document.getElementById('upload-media').classList.remove('show');
-                document.getElementById('import-repo-media').classList.add('show');
-                document.getElementById('import-repo-media').classList.add('active');
-                document.getElementById('upload-media-tab').setAttribute('aria-selected', 'false');
-                document.getElementById('import-repo-media-tab').setAttribute('aria-selected', 'true');
-                document.querySelector('#repo-uuid').value = record.media;
-                document.querySelector('#is-repo-item').value = 1;
-                await helperMediaModule.get_repo_item_data(null);
-
-                if (record.item_type === 'image') {
-                    helperMediaModule.set_alt_text(record);
-                }
-            }
-
-            if (record.is_kaltura_item === 1) {
-
-                document.getElementById('upload-media-tab').classList.remove('active');
-                document.getElementById('import-audio-video-tab').classList.add('active');
-                document.getElementById('upload-media').classList.remove('active');
-                document.getElementById('upload-media').classList.remove('show');
-                document.getElementById('import-audio-video').classList.add('show');
-                document.getElementById('import-audio-video').classList.add('active');
-                document.getElementById('upload-media-tab').setAttribute('aria-selected', 'false');
-                document.getElementById('import-audio-video-tab').setAttribute('aria-selected', 'true');
-                document.querySelector('#audio-video').value = record.media;
-                document.querySelector('#is-kaltura-item').value = 1;
-                await helperMediaModule.get_kaltura_item_data(null);
-
-                document.querySelector('#kaltura-item-type').value = record.item_type;
-                document.querySelector('#item-type').value = 'kaltura';
-            }
-
-            document.querySelector('#item-mime-type').value = helperModule.unescape(record.mime_type);
-            document.querySelector('#item-media-thumbnail-image-display').innerHTML = thumbnail_fragment;
-            document.querySelector('#item-media').value = record.media;
-            document.querySelector('#item-media-prev').value = record.media;
-        }
-
-        if (!record.thumbnail === null && record.thumbnail.length > 0) {
-
-            thumbnail_url = EXHIBITS_ENDPOINTS.exhibits.exhibit_media.get.endpoint.replace(':exhibit_id', record.is_member_of_exhibit).replace(':media', record.thumbnail);
-            thumbnail_fragment = `<p><img src="${thumbnail_url}" height="200" ></p>`;
-            document.querySelector('#item-thumbnail-image-display').innerHTML = thumbnail_fragment;
-            document.querySelector('#item-thumbnail-filename-display').innerHTML = `<span style="font-size: 11px">${record.thumbnail}</span>`;
-            document.querySelector('#item-thumbnail').value = record.thumbnail;
-            document.querySelector('#item-thumbnail-image-prev').value = record.thumbnail;
-            document.querySelector('#item-thumbnail-trash').style.display = 'inline';
-        }
-    };
-    */
 
     /**
      * Initializes common code for media card
