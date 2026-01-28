dd663cdd6a9fb83dbc060141d6e58c5f10a5077f
Fernando Reyes <fernando.reyes@du.edu>
2026-01-28 16:38:14 -0700

    added license info to js files

--- public/app/media-library/modals.module.js
+++ public/app/media-library/modals.module.js
@@ -1,10 +1,17 @@
 /**
- * Media Modals Module
- * 
- * Handles modal windows for the media library, including the
- * uploaded media details modal that appears after file uploads.
- * 
- * Copyright 2023 University of Denver
+ * Copyright 2026 University of Denver
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 const mediaModalsModule = (function() {
