81b57e1e76745a78a72e97b6d256f5671ed48d23
Fernando Reyes <fernando.reyes@du.edu>
2026-04-13 20:55:52 -0600

    removed title requirement from timeline schema check

--- exhibits/schemas/exhibit_timeline_update_record_schema.js
+++ exhibits/schemas/exhibit_timeline_update_record_schema.js
@@ -22,7 +22,6 @@ module.exports = () => {
 
     return {
         is_member_of_exhibit: {type: 'string'},
-        uuid: {type: 'string'},
-        title: {type: 'string'}
+        uuid: {type: 'string'}
     };
 };
