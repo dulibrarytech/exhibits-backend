95e36f7476c91fcedb68fc15906b63f8263499dc
freyesdulib <freyesdulib@gmail.com>
2026-08-10 12:58:32 -0700

    Added preview menu item link to grid item edit form; fixed grid text details nav and media picker init noise

--- dashboard/controller.js
+++ dashboard/controller.js
@@ -216,6 +216,7 @@ const NAV_CONFIGS = {
     },
 
     grid_item_details: {
+        show_preview: true,
         back: {
             id: 'back-to-items',
             label: 'Back to Grid Items',
